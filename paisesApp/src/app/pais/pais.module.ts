0fbc597b814b5f51e442e4c2ee2fced396aea5a2
unknown <gabbjuncos@gmail.com>
2022-06-08 13:36:09 -0300

    LISTO HASTA 8.6

---
+++ paisesApp/src/app/pais/pais.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
+import { PorRegionComponent } from './pages/por-region/por-region.component';
+import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { PorPaisComponent } from './pages/por-pais/por-pais.component';
+
+
+
+@NgModule({
+  declarations: [
+    PorCapitalComponent,
+    PorRegionComponent,
+    VerPaisComponent,
+    PorPaisComponent
+  ],
+  exports: [
+    PorCapitalComponent,
+    PorRegionComponent,
+    VerPaisComponent,
+    PorPaisComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PaisModule { }
