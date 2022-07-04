a6e219331c6d344812adb83f3d89cff57ee1344f
unknown <gabbjuncos@gmail.com>
2022-07-04 10:18:35 -0300

    LISTO SECCION 9

--- paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { Country } from '../../interfaces/pais.interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+    li{
+      cursor: pointer;
+    }
+    `
   ]
 })
 export class PorPaisComponent{
@@ -14,10 +19,20 @@ export class PorPaisComponent{
   hayError:boolean = false;
   // para tener la lista de paises
   paises : Country[] = [];
+  // para paises sugeridos
+
+  paisesSugeridos : Country[] = [];
+// por defecto el falsa
+  mostrarSugerencias : boolean = false;
+
 
   constructor(private paisService:PaisService) { }
 
   buscar(termino:string){
+    // y ahora tengo que ocultar la barra de sugerncias
+    this.mostrarSugerencias = false;
+
+
     this.hayError = false;
     // el this.termino va a ser igual al termino que se recibe como argumento
     // es el que viene de input
@@ -42,8 +57,27 @@ export class PorPaisComponent{
 
   sugerencias(termino:string){
     this.hayError = false;
-    //TODO: crear sugerencias 
+    this.termino = termino;
+    this.mostrarSugerencias= true;
+
+    
+
+    this.paisService.buscarPais(termino)
+    // este arreglo se va a encargar de mostrar las sugerencias y con el splice corto la lista desde la 3ra posiscionm para mostrar 3 sugerencias nomas
+      .subscribe(
+        paises => this.paisesSugeridos = paises.splice(0,3),
+        // para pasar una lista vacia
+        (err) => this.paisesSugeridos = []
+        );
   }
+
+  buscarSugerido(termino:string){
+    this.buscar(termino);
+
+    
+  }
+
+
 
 
 }
